3fe2af68a8a2cfaf28aea428d33dc590ac7c01c6
hvuhsg5@gmail.com <hvuhsg6@gmail.com>
2021-03-31 20:57:49 +0300

    First upload

---
+++ qwhale_logs_client/main.py
@@ -0,0 +1,31 @@
+import requests
+from json import dumps
+from urllib.parse import urljoin
+from logging import Handler, LogRecord
+
+__all__ = ["QWhaleLogsHandler"]
+
+SERVICE_URL = "http://127.0.0.1:8000/"
+
+
+class QWhaleLogsHandler(Handler):
+    def __init__(self, token: str, batch_site: int = 100):
+        self.token = token
+        self.logs = []
+        self.batch = None
+        self.batch_size = batch_site
+        super().__init__()
+
+    def __upload(self):
+        payload = {"token": self.token}
+        try:
+            requests.put(urljoin(SERVICE_URL, f"/api/logs"), params=payload, data=dumps({"logs": self.batch}))
+        except (ConnectionError, ValueError) as EX:
+            pass
+
+    def emit(self, record: LogRecord) -> None:
+        self.logs.append(record.__dict__)
+        if len(self.logs) >= self.batch_size:
+            self.batch = self.logs.copy()
+            self.logs.clear()
+            self.__upload()
